1ad95c16e9e304d0921edf6f05f35d0e1c5bbfbe
Daw-Chih Liou <dawochih.liou@gmail.com>
2021-12-15 08:35:34 +0100

    feat: add memoization

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useState, memo, useCallback, useMemo } from 'react';
 import { initialTodos, getUpdated, getFiltered } from '../../utils/todo';
-import Todo from '../Todo';
+import UnmemoizedTodo from '../Todo';
 import AddTodo from '../AddTodo';
 import { VisibilityStatus } from '../../types';
 import styles from './styles.module.css';
 import { useCounter } from '../../hooks/useCounter';
+
+// ✨
+const Todo = memo(UnmemoizedTodo);
 
 interface TodoListProps {
   visibility: VisibilityStatus;
@@ -13,19 +16,22 @@ interface TodoListProps {
 
 export default function TodoList({ visibility, themeColor }: TodoListProps) {
   const [todos, setTodos] = useState(initialTodos);
-  const handleChange = (todo: string) =>
-    setTodos((todos) => getUpdated(todos, todo));
-  const filtered = getFiltered(todos, visibility);
-  const count = useCounter();
 
+  // ✨
+  const handleChange = useCallback(
+    (todo: string) => setTodos((todos) => getUpdated(todos, todo)),
+    [],
+  );
+
+  // ✨
+  const filtered = useMemo(() => getFiltered(todos, visibility), [
+    todos,
+    visibility,
+  ]);
+  // const filtered = getFiltered(todos, visibility);
   return (
     <div>
-      <div className={styles.wrapper}>
-        <code>
-          getFiltered() was called <span className={styles.label}>{count}</span>{' '}
-          times
-        </code>
-      </div>
+      <div className={styles.wrapper}></div>
       <ul className={styles.list}>
         {filtered.map((todo) => (
           <Todo key={todo.id} todo={todo} onChange={handleChange} />
